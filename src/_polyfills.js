b8fbc123c77270bf1afea9cf5ad49712b7380838
shamblesides <nigel.mark.nelson@gmail.com>
2014-11-24 21:51:25 -0700

    Math.sign polyfill

--- src/_polyfills.js
+++ src/_polyfills.js
@@ -1,5 +1,11 @@
+// requestAnimationFrame
 window.requestAnimationFrame =
   window.requestAnimationFrame ||
   window.mozRequestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.oRequestAnimationFrame;
+
+// Math.sign polyfill
+Math.sign = function sign(x) {
+  return !(x= parseFloat(x)) ? x : x > 0 ? 1 : -1;
+};
